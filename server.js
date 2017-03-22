b6388f05d12124b66d6b7fd8ea1a226c46ff28e2
Zefei Guo <guozf@rd.netease.com>
2017-03-22 14:09:40 +0800

    test send message

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 const cluster = require('cluster');
 const http = require('http');
-const numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
 
-  // Fork workers.
+  // Keep track of http requests
+  let numReqs = 0;
+  setInterval(() => {
+    console.log(`numReqs = ${numReqs}`);
+  }, 1000);
+
+  // Count requests
+  function messageHandler(msg) {
+    if (msg.cmd && msg.cmd === 'notifyRequest') {
+      numReqs += 1;
+    }
+  }
+
+  // Start workers and listen for messages containing notifyRequest
+  const numCPUs = require('os').cpus().length;
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
+  for (const id in cluster.workers) {
+    cluster.workers[id].on('message', messageHandler);
+  }
+
 } else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  http.createServer((req, res) => {
+
+  // Worker processes have a http server.
+  http.Server((req, res) => {
     res.writeHead(200);
     res.end('hello world\n');
-  }).listen(8000);
 
-  console.log(`Worker ${process.pid} started`);
+    // notify master about the request
+    process.send({ cmd: 'notifyRequest' });
+  }).listen(8000);
 }
